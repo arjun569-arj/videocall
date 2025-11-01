08598fc31643745259ff716db5285001c519289d
Arjun reddy <arjun569@gmail.com>
2025-11-02 00:40:49 +0530

    first commit

---
+++ backend/src/env.js
@@ -0,0 +1,6 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+export const env = {
+  NODE_ENV: process.env.NODE_ENV,
+};
---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "path";
+import { env } from "./env.js";
+
+const app = express();
+
+const __dirname = path.resolve();
+
+if (env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../fronthend/dist")));
+  app.get("/{*any}", function (_, res) {
+    res.sendFile(path.join(__dirname + "../../fronthend/dist/index.html"));
+  });
+}
+
+app.get("/hello", (req, res) => {
+  return res.send("Hello World!");
+});
+
+app.listen(3000);
