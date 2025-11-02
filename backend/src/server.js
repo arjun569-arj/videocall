0a448545b690e85f6ffe23a960e26fc5f28927e2
Arjun reddy <arjun569@gmail.com>
2025-11-02 12:24:46 +0530

    32

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,24 +1,27 @@
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
+import { fileURLToPath } from "url";
 
-dotenv.config(); // Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 
-const __dirname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const frontendDistPath = path.resolve(__dirname, "../../frontend/dist");
 
 app.get("/hello", (req, res) => {
   return res.status(200).json({ message: "Hello World" });
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(frontendDistPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendDistPath, "index.html"));
+  });
+}
 
 app.listen(process.env.PORT, () =>
   console.log("Server is running on port:", process.env.PORT)
