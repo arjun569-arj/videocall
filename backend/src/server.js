c2390377ac109f68603fcb5667140a89786e8ecf
Arjun reddy <arjun569@gmail.com>
2025-11-02 12:28:34 +0530

    change in server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,27 +1,24 @@
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
-import { fileURLToPath } from "url";
 
-dotenv.config();
+dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const frontendDistPath = path.resolve(__dirname, "../../frontend/dist");
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.get("/hello", (req, res) => {
   return res.status(200).json({ message: "Hello World" });
 });
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(frontendDistPath));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendDistPath, "index.html"));
-  });
-}
 
 app.listen(process.env.PORT, () =>
   console.log("Server is running on port:", process.env.PORT)
