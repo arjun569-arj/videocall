c86133b0ee072cb765a58a146444f6054a20fd8f
Arjun reddy <arjun569@gmail.com>
2025-11-02 22:19:00 +0530

    server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,28 @@
 import express from "express";
 import path from "path";
-import dotenv from "dotenv";
-import { connectDB } from "./utils/connectDB.js";
-import { serve } from "inngest/express";
-import { inngest, functions } from "./utils/inngest.js";
 import cors from "cors";
-
-dotenv.config(); // Load environment variables from .env file
+import { serve } from "inngest/express";
+import { connectDB } from "./utils/connectDB.js";
+import { inngest, functions } from "./utils/inngest.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
 const __dirname = path.resolve();
 
+// middleware
+app.use(express.json());
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
+});
+
+// make our app ready for deployment
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -19,19 +30,16 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
-);
-app.use("/api/inngest", serve({ client: inngest, functions }));
-app.get("/hello", (req, res) => {
-  return res.status(200).json({ message: "Hello World" });
-});
 
-app.listen(process.env.PORT, async () => {
-  await connectDB();
-  console.log("Server is running on port:", process.env.PORT);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(process.env.PORT, () =>
+      console.log("Server is running on port:", process.env.PORT)
+    );
+  } catch (error) {
+    console.error("💥 Error starting the server", error);
+  }
+};
+
+startServer();
