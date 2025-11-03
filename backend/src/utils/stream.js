dba341c02a535045668423ee86d35f4fc2a35a31
Arjun reddy <arjun569@gmail.com>
2025-11-03 19:19:33 +0530

    update

--- backend/src/utils/stream.js
+++ backend/src/utils/stream.js
@@ -1,5 +1,4 @@
 import { StreamChat } from "stream-chat";
-import { StreamClient } from "@stream-io/node-sdk";
 
 const apiKey = process.env.STREAM_API_KEY;
 const apiSecret = process.env.STREAM_API_SECRET;
@@ -9,7 +8,6 @@ if (!apiKey || !apiSecret) {
 }
 
 export const chatClient = StreamChat.getInstance(apiKey, apiSecret); // will be used chat features
-export const streamClient = new StreamClient(apiKey, apiSecret); // will be used for video calls
 
 export const upsertStreamUser = async (userData) => {
   try {
